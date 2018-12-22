8a769fb15d34d682630491567e4193a40dd8a87f
BeneficialPickle <40075115+BeneficialPickle@users.noreply.github.com>
2018-12-22 17:16:13 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,18 +3,29 @@ import random
 import subprocess
 import os
 
-numOfFiles = 8
-array = []
+userInput = ""
 questions = ""
+array = []#array of one random question from each assignment
 
-for i in range(numOfFiles):
-    file = open('C:/Users/poote/Documents/MATH147/LaTeX - new/A' + str((i + 1)) + '.tex')
-    temp = re.findall(r'\\begin{question}(.*?)\\end{question}', file.read(), re.S)
-    rand = random.randint(0, len(temp) - 1)
-    array.append(temp[rand])
+title = "title"
+save = input("Enter the file path to where you want your pdf saved.")
+#title = input("Enter the title of the pdf.")
+while userInput != "stop":
+    userInput = input("Please enter the file location of your tex file, or enter 'stop'.")
+    try:
+        file = open(userInput)
+        #something that finds the section in the latex file - SO
+        temp = re.findall(r'\\begin{question}(.*?)\\end{question}', file.read(), re.S)
+        rand = random.randint(0, len(temp) - 1)
+        array.append(temp[rand])
+    except:
+        if userInput != stop:
+            print("An error occured, please try again\n")
+        continue
 
 for i in array:
     questions += "\\begin{question}\n" + i[5:None] + "\\end{question}\n\n"
+    #wrap the contents in the question environment
 
 document = r'''\documentclass[12pt]{article}
 \usepackage{tasks}
@@ -32,17 +43,16 @@ document = r'''\documentclass[12pt]{article}
 \begin{document}
 
 \begin{center}
-\textbf{\large Math 147 (Davidson: section 2) Randomized Assignments}\\[1ex]
+\textbf{\large ''' + title + '''}\\[1ex]
 \end{center}
 
 ''' + questions + "\end{document}"
 
-with open('randomized_147.tex', 'w') as f:
-    f.write(document)
+with open('randomized_' + title + '.tex', 'w') as f:
+    f.write(document)#create a new file and shove the latex code in
 
-print(document)
+os.system(save + "pdflatex randomized_" + title + ".tex")#compile the code with the command pdflatex
 
-#os.system("pdflatex randomized_147.tex")
-
-#os.unlink("randomized_147.tex")
-#os.unlink("randomized_147.log")
+#remove the non-pdf files that come with compiling
+os.unlink(save + "randomized_" + title + ".tex")
+os.unlink(save + "randomized_" + title + ".log")
