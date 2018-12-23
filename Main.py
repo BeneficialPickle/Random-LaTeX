b7c68d1d046a936af8fa03a2b78bbb294c25f130
BeneficialPickle <40075115+BeneficialPickle@users.noreply.github.com>
2018-12-23 17:32:48 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,58 +1,90 @@
-import re
+from tkinter import *
+from tkinter.filedialog import askopenfilename
 import random
-import subprocess
 import os
+import re
 
-userInput = ""
-questions = ""
-array = []#array of one random question from each assignment
+root = Tk()
+files = []
+labels = []
 
-title = "title"
-save = input("Enter the file path to where you want your pdf saved.")
-#title = input("Enter the title of the pdf.")
-while userInput != "stop":
-    userInput = input("Please enter the file location of your tex file, or enter 'stop'.")
+def OpenFile():
+    path = askopenfilename(initialdir="C:/Users/poote/Desktop/",
+                           filetypes=(("LaTeX Files", "*.tex"), ("All Files", "*.*")),
+                           title="Choose a file."
+                           )
     try:
-        file = open(userInput)
-        #something that finds the section in the latex file - SO
-        temp = re.findall(r'\\begin{question}(.*?)\\end{question}', file.read(), re.S)
-        rand = random.randint(0, len(temp) - 1)
-        array.append(temp[rand])
+        files.append(path)
+        temp = Label(root, text=path + " added.")
+        temp.pack()
+        temp.place(relx=0.5, rely=0.6, anchor=CENTER)
+        for label in labels:
+            label.destroy()
+        labels.append(temp)
     except:
-        if userInput != stop:
-            print("An error occured, please try again\n")
-        continue
+        print("No file exists")
 
-for i in array:
-    questions += "\\begin{question}\n" + i[5:None] + "\\end{question}\n\n"
-    #wrap the contents in the question environment
+def CreatePdf():
+    questions = []
+    latexCode = ""
+    title = "Title"
+    save = ""
+    for f in files:
+        try:
+            file = open(f)
+            temp = re.findall(r'\\begin{question}(.*?)\\end{question}', file.read(), re.S)
+            rand = random.randint(0, len(temp) - 1)
+            questions.append(temp[rand])
+        except:
+            print("There was an error in creating the PDF.")
+            continue
+    for q in questions:
+        latexCode += "\\begin{question}\n" + q[5:None] + "\\end{question}\n\n"
+    document = r'''\documentclass[12pt]{article}
+    \usepackage{tasks}
+    \usepackage{exsheets}
+    \usepackage{graphicx}
+    \usepackage{fullpage}
+    \usepackage{multicol}
+    \usepackage{amsmath,amsthm,amssymb}
+    \SetupExSheets[question]{type=exam}
 
-document = r'''\documentclass[12pt]{article}
-\usepackage{tasks}
-\usepackage{exsheets}
-\usepackage{graphicx}
-\usepackage{fullpage}
-\usepackage{multicol}
-\usepackage{amsmath,amsthm,amssymb}
-\SetupExSheets[question]{type=exam}
+    \newtheorem{theorem}{Theorem}
+    \newtheorem{corollary}{Corollary}[theorem]
+    \newtheorem{lemma}[theorem]{Lemma}
 
-\newtheorem{theorem}{Theorem}
-\newtheorem{corollary}{Corollary}[theorem]
-\newtheorem{lemma}[theorem]{Lemma}
+    \begin{document}
 
-\begin{document}
+    \begin{center}
+    \textbf{\large ''' + title + '''}\\[1ex]
+    \end{center}
+    ''' + latexCode + "\end{document}"
 
-\begin{center}
-\textbf{\large ''' + title + '''}\\[1ex]
-\end{center}
+    with open(save + 'randomized_' + title + '.tex', 'w') as f:
+        f.write(document)  # create a new file and shove the latex code in
 
-''' + questions + "\end{document}"
+    filename = save + "randomized_" + title + ".tex"
 
-with open('randomized_' + title + '.tex', 'w') as f:
-    f.write(document)#create a new file and shove the latex code in
+    os.system("pdflatex", filename)  # compile the code with the command pdflatex
 
-os.system(save + "pdflatex randomized_" + title + ".tex")#compile the code with the command pdflatex
+    # remove the non-pdf files that come with compiling
+    try:
+        os.unlink(save + "\\randomized_" + title + ".tex")
+        os.unlink(save + "\\randomized_" + title + ".log")
+    except:
+        print("Encountered an error generating the pdf.")
 
-#remove the non-pdf files that come with compiling
-os.unlink(save + "randomized_" + title + ".tex")
-os.unlink(save + "randomized_" + title + ".log")
+
+Title = root.title("LaTeX Problem Set Generator")
+Size = root.geometry('400x400')
+
+# Button
+
+chooseFile = Button(root, text="Choose Files", command=OpenFile)
+stop = Button(root, text="Compile PDF", command=CreatePdf)
+chooseFile.pack()
+stop.pack()
+chooseFile.place(relx=0.5, rely=0.4, anchor = CENTER)
+stop.place(relx=0.5, rely=0.5, anchor = CENTER)
+
+root.mainloop()
